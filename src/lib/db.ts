36328bb44e7355e94d3da769a372d07552285969
mwidman <2118673+mwidman@users.noreply.github.com>
2023-12-13 12:00:02 -0800

    fix: Adding jsdoc for createShortenedLink and types for short-hash

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -5,6 +5,12 @@ export const baseUrl = 'http://localhost:5173/api/links/';
 
 export const links = new Map();
 
+/**
+ * Creates a shortened link and returns it to the user.
+ * @param keyword A keyword that represents the link. If ommitted, a hash is generated instead.
+ * @param link  The original link that will be associated with the key.
+ * @returns 
+ */
 export async function createShortenedLink(keyword: string, link: string): Promise<string> {
 
     let key = keyword;
